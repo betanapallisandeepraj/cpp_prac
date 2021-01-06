6f1d87cb6f78ea19a6444fe546709ad63c04d0d9
Sandeep Raj B <betanapallisandeepraj@gmail.com>
2021-01-06 22:05:40 +0800

    hello world program added

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("Hello World");
+    std::cout << "hello world" << std::endl;
+    cout << "skdf";
+    return 0;
+}
+
